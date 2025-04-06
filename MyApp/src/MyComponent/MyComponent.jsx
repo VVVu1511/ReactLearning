23d33d49b37873e88c7051aa6be90c41daa60dd3
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-04-06 10:08:24 +0700

    useState

--- MyApp/src/MyComponent/MyComponent.jsx
+++ MyApp/src/MyComponent/MyComponent.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 
 function MyComponent(){
@@ -91,57 +91,101 @@ function MyComponent(){
     //     </div>
     // );
 
-    const [cars, setCars] = useState([]);
-    const [carYear, setCarYear] = useState(new Date().getFullYear());
-    const [carMake, setCarMake] = useState("");
-    const [carModel, setCarModel] = useState("");
+    // const [cars, setCars] = useState([]);
+    // const [carYear, setCarYear] = useState(new Date().getFullYear());
+    // const [carMake, setCarMake] = useState("");
+    // const [carModel, setCarModel] = useState("");
 
-    function handleAddCar(){
-        const new_car = {year: carYear, make: carMake, model: carModel};
-        setCars(c => [...c,new_car]);
+    // function handleAddCar(){
+    //     const new_car = {year: carYear, make: carMake, model: carModel};
+    //     setCars(c => [...c,new_car]);
 
-        setCarYear(new Date().getFullYear());
-        setCarMake("");
-        setCarModel("");
-    }
+    //     setCarYear(new Date().getFullYear());
+    //     setCarMake("");
+    //     setCarModel("");
+    // }
 
-    function handleRemoveCar(index){
-        setCars(c => c.filter((_,i) => i != index));
-    }
+    // function handleRemoveCar(index){
+    //     setCars(c => c.filter((_,i) => i != index));
+    // }
 
-    function handleYearChange(event){
-        setCarYear(event.target.value);
-    }
+    // function handleYearChange(event){
+    //     setCarYear(event.target.value);
+    // }
 
-    function handleMakeChange(event){
-        setCarMake(event.target.value);
-    }
+    // function handleMakeChange(event){
+    //     setCarMake(event.target.value);
+    // }
 
-    function handleModelChange(event){
-        setCarModel(event.target.value);
+    // function handleModelChange(event){
+    //     setCarModel(event.target.value);
+    // }
+
+    // return (
+    //     <div>
+    //         <h2>List of Car Objects</h2>
+    //         <ul>
+    //             {cars.map((car, index) => <li onClick={() => handleRemoveCar(index)} key={index}>{car.year} {car.make} {car.model}</li>)}
+    //         </ul>
+
+    //         <input type="number" value={carYear} onChange={handleYearChange}></input>
+    //         <br></br>
+
+    //         <input type="text" value={carMake} onChange={handleMakeChange}
+    //                 placeholder="Enter car make"></input>
+    //         <br></br>
+
+    //         <input type="text" value={carModel} onChange={handleModelChange}
+    //                 placeholder="Enter car model"></input>
+    //         <br></br>
+
+    //         <button onClick={handleAddCar}>Add Car</button>
+    //     </div>
+    // );
+
+    // const [count, setCount] = useState(0);
+    // const [color, setColor] = useState("green");
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        console.log('Event listener');
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            console.log('Event listener removed');
+        }
+    }, []);
+
+    // useEffect(() => {
+    //     document.title = `Count: ${count} ${color}`;
+    // },[count,color]);
+
+    // function addCount(){
+    //     setCount(c => c + 1);
+    // }
+
+    // function subtractCount(){
+    //     setCount(c => c - 1);
+    // }
+
+    // function changeColor(){
+    //     setColor(c => c === "green" ? "red" : "green");
+    // }
+
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
     }
 
     return (
-        <div>
-            <h2>List of Car Objects</h2>
-            <ul>
-                {cars.map((car, index) => <li onClick={() => handleRemoveCar(index)} key={index}>{car.year} {car.make} {car.model}</li>)}
-            </ul>
+        <>
+            <p>Window Width: {width} px</p>
+            <p>Window Height: {height} px</p>
+        </>
+    )
 
-            <input type="number" value={carYear} onChange={handleYearChange}></input>
-            <br></br>
-
-            <input type="text" value={carMake} onChange={handleMakeChange}
-                    placeholder="Enter car make"></input>
-            <br></br>
-
-            <input type="text" value={carModel} onChange={handleModelChange}
-                    placeholder="Enter car model"></input>
-            <br></br>
-
-            <button onClick={handleAddCar}>Add Car</button>
-        </div>
-    );
 }
 
 export default MyComponent
